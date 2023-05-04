0ef130a4cbdbacb51392ea2d86f32cd74ee70d21
ChalindKodikara <chalindu.18@cse.mrt.ac.lk>
2023-05-04 17:58:06 +0530

    Add a fix to pretrained model

--- distributed-pre-trained-graphsage/models/supervised.py
+++ distributed-pre-trained-graphsage/models/supervised.py
@@ -2,14 +2,13 @@ import logging
 import sys
 
 logging.basicConfig(
-    level=logging.INFO, 
+    level=logging.INFO,
     format='%(asctime)s : [%(levelname)s]  %(message)s',
     handlers=[
         logging.FileHandler('supervised.log'),
         logging.StreamHandler(sys.stdout)
     ]
 )
-
 
 import stellargraph as sg
 from stellargraph.data import EdgeSplitter
@@ -22,7 +21,6 @@ import tensorflow as tf
 from tensorflow import keras
 import random as python_random
 
-
 from sklearn import preprocessing, feature_extraction, model_selection
 import os
 import numpy as np
@@ -33,12 +31,13 @@ tf.random.set_seed(42)
 np.random.seed(42)
 python_random.seed(42)
 
+
 class Model:
 
-    def __init__(self,nodes,edges):
+    def __init__(self, nodes, edges):
         self.model = None
 
-        self.nodes =  nodes
+        self.nodes = nodes
         self.edges = edges
 
         self.graph_train = None
@@ -47,38 +46,33 @@ class Model:
         self.train_flow = None
         self.test_flow = None
 
-    def initialize(self, test_edges, **hyper_params):
+    def initialize(self, **hyper_params):
 
-        if(not "batch_size" in hyper_params.keys()):
+        if (not "batch_size" in hyper_params.keys()):
             batch_size = 40
-        if(not "layer_sizes" in hyper_params.keys()):
+        if (not "layer_sizes" in hyper_params.keys()):
             num_samples = [20, 10]
-        if(not "num_samples" in hyper_params.keys()):
+        if (not "num_samples" in hyper_params.keys()):
             layer_sizes = [15, 15]
-        if(not "bias" in hyper_params.keys()):
+        if (not "bias" in hyper_params.keys()):
             bias = True
-        if(not "dropout" in hyper_params.keys()):
+        if (not "dropout" in hyper_params.keys()):
             dropout = 0.1
-        if(not "lr" in hyper_params.keys()):
+        if (not "lr" in hyper_params.keys()):
             lr = 1e-2
 
+        # Get future edges for testing
+        test_edges = self.edges.iloc[int(self.edges.shape[0] * 0.9):]
+
+        self.edges = self.edges.iloc[:int(self.edges.shape[0] * 0.9)]
+
+        graph_test = sg.StellarGraph(nodes=self.nodes, edges=test_edges)
+        edge_splitter_test = EdgeSplitter(graph_test)
+        self.graph_test, edge_ids_test, edge_labels_test = edge_splitter_test.train_test_split(
+            p=0.1, method="global", keep_connected=False, seed=2023
+        )
+
         graph = sg.StellarGraph(nodes=self.nodes, edges=self.edges)
-
-        if test_edges != 0:
-            graph_test = sg.StellarGraph(nodes=self.nodes, edges=test_edges)
-            edge_splitter_test = EdgeSplitter(graph_test)
-            self.graph_test, edge_ids_test, edge_labels_test = edge_splitter_test.train_test_split(
-                p=0.1, method="global", keep_connected=False, seed=2023
-            )
-            edge_splitter_train = EdgeSplitter(graph)
-
-        else:
-            # Test split
-            edge_splitter_test = EdgeSplitter(graph)
-            self.graph_test, edge_ids_test, edge_labels_test = edge_splitter_test.train_test_split(
-                p=0.1, method="global", keep_connected=False, seed=2023
-            )
-            edge_splitter_train = EdgeSplitter(self.graph_test)
 
         # Test split
         # edge_splitter_test = EdgeSplitter(graph)
@@ -87,6 +81,7 @@ class Model:
         # )
 
         # Train split
+        edge_splitter_train = EdgeSplitter(graph)
         self.graph_train, edge_ids_train, edge_labels_train = edge_splitter_train.train_test_split(
             p=0.1, method="global", keep_connected=False, seed=2023
         )
@@ -115,22 +110,23 @@ class Model:
         self.model.compile(
             optimizer=keras.optimizers.Adam(lr=lr),
             loss=keras.losses.binary_crossentropy,
-            metrics=[keras.metrics.BinaryAccuracy(),keras.metrics.Recall(),keras.metrics.AUC(),keras.metrics.Precision()],
+            metrics=[keras.metrics.BinaryAccuracy(), keras.metrics.Recall(), keras.metrics.AUC(),
+                     keras.metrics.Precision()],
         )
 
         # return number of training and testing examples
-        return edge_ids_train.shape[0],edge_ids_test.shape[0]
+        return edge_ids_train.shape[0], edge_ids_test.shape[0]
 
-    def set_weights(self,weights):
+    def set_weights(self, weights):
         self.model.set_weights(weights)
 
     def get_weights(self):
         return self.model.get_weights()
 
-    def fit(self, epochs = 20):
+    def fit(self, epochs=20):
         history = self.model.fit(self.train_flow, epochs=epochs, verbose=1)
-        return self.model.get_weights(),history
-    
+        return self.model.get_weights(), history
+
     def evaluate(self):
         train_metrics = self.model.evaluate(self.train_flow)
         test_metrics = self.model.evaluate(self.test_flow)
@@ -152,11 +148,11 @@ class Model:
         # y = self.model.predict(test_flow)
         x = 5
 
-if __name__ == "__main__":
 
-    #path_weights = "./weights/weights_cora.npy"
-    #path_nodes = "./data/4_nodes_0.csv"
-    #path_edges = "./data/4_edges_0.csv"
+if __name__ == "__main__":
+    # path_weights = "./weights/weights_cora.npy"
+    # path_nodes = "./data/4_nodes_0.csv"
+    # path_edges = "./data/4_edges_0.csv"
 
     arg_names = [
         'path_weights',
@@ -167,35 +163,38 @@ if __name__ == "__main__":
 
     args = dict(zip(arg_names, sys.argv[1:]))
 
-    nodes = pd.read_csv(args["path_nodes"],index_col=0)
-    #nodes = nodes.astype("float32")
+    nodes = pd.read_csv(args["path_nodes"], index_col=0)
+    # nodes = nodes.astype("float32")
 
     edges = pd.read_csv(args["path_edges"])
-    #edges = edges.astype({"source":"uint32","target":"uint32"})
+    # edges = edges.astype({"source":"uint32","target":"uint32"})
 
-    logging.warning('####################################### New Training Session #######################################')
+    logging.warning(
+        '####################################### New Training Session #######################################')
 
-    model = Model(nodes,edges)
+    model = Model(nodes, edges)
     model.initialize()
 
     logging.info('Training started!')
     start = timer()
 
-    new_weights,history = model.fit(int(args["epochs"]))
+    new_weights, history = model.fit(int(args["epochs"]))
 
     end = timer()
     logging.info('Training done!')
 
-    elapsed_time = end -start
+    elapsed_time = end - start
 
     # Save weights
-    np.save(args['path_weights'],new_weights)
+    np.save(args['path_weights'], new_weights)
 
     eval = model.evaluate()
 
     f1_train = (2 * eval[0][2] * eval[0][4]) / (eval[0][2] + eval[0][4])
     f1_test = (2 * eval[1][2] * eval[1][4]) / (eval[1][2] + eval[1][4])
 
-    logging.info('Training set : accuracy - %s, recall - %s, AUC - %s, F1 - %s, precision - %s',eval[0][1],eval[0][2],eval[0][3],f1_train,eval[0][4])
-    logging.info('Testing set : accuracy - %s, recall - %s, AUC - %s, F1 - %s, precision - %s',eval[1][1],eval[1][2],eval[1][3],f1_test,eval[1][4])
+    logging.info('Training set : accuracy - %s, recall - %s, AUC - %s, F1 - %s, precision - %s', eval[0][1], eval[0][2],
+                 eval[0][3], f1_train, eval[0][4])
+    logging.info('Testing set : accuracy - %s, recall - %s, AUC - %s, F1 - %s, precision - %s', eval[1][1], eval[1][2],
+                 eval[1][3], f1_test, eval[1][4])
     logging.info('Elapsed time : %s seconds', elapsed_time)
